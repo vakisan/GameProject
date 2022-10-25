c7336f94f7dd10561421fbf39bfe56ba6ad30db6
Vakisan Manoharan <vakisan@hotmail.co.uk>
2022-10-25 14:40:25 +0100

    Created Terrain and Sea

---
+++ Assets/Cube.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
